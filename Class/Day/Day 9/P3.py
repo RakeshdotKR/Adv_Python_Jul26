11f14771c793c236fbca012e9d2671e6c0c7e8d4
rakeshkr-abc <rakesh.kr@abc.courses>
2026-07-17 18:48:14 +0530

    Global Interpreter Lock (GIL)

--- Class/Day/Day 9/P3.py
+++ Class/Day/Day 9/P3.py
@@ -1,6 +1,7 @@
 # Global Interpreter Lock (GIL)
 # GIL is a mutex, prevents multiple native threads from executing python bytecode
-
+# GIL like a single key to a room, only one person can be inside the room at 
+# any given point of timw
 import time
 from threading import Thread
 
